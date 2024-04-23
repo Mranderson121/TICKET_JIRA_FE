61c153c69b97e67205329245babb1362b4c13548
andrea <andreaochi9@hotmail.it>
2024-04-23 17:44:29 +0200

    prova

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
---
+++ src/app/interfaces/ticket.ts
@@ -0,0 +1,7 @@
+export interface Ticket{
+id:number
+descrizione:string
+stato:string
+commento:string
+
+}
