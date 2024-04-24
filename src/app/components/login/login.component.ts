57babcd92e2c1c0c857e1699cb9d23cbd590c3a1
andrea <andreaochi9@hotmail.it>
2024-04-24 09:05:04 +0200

    aggiunto component dashboard e servies

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm!: FormGroup;
+  failLoginMsg:boolean=false;
+  constructor (
+    private router:Router
+  ){}
+  ngOnInit(): void {
+    this.loginForm=new FormGroup({
+      username: new FormControl(null,Validators.required),
+      password:new FormControl(null,Validators.required),
+    });
+  }
+
+  onSubmit(){
+    
+  }
 
 }
