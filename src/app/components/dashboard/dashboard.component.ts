5ce34eb7663bdf53dc7664fc7829e6ea115ab37c
zeno <giuseppezeno97@gmail.com>
2024-04-30 18:27:14 +0200

    aggiunt ometodo per la get dell'impaginazione

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -9,10 +9,7 @@ import { TicketService } from 'src/app/services/ticket.service';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  constructor(
-    private route: ActivatedRoute,
-    private service: TicketService
-  ) {}
+  constructor(private route: ActivatedRoute, private service: TicketService) {}
 
   employeeId!: number;
   taskmanager?: number;
@@ -22,12 +19,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.buttons = [];
-
-    this.button = {
-      value: 0,
-      min: 0,
-      max: 0,
-    };
+    this.button = { value: 0, min: 0, max: 0 };
 
     this.route.queryParams.subscribe((params) => {
       this.employeeId = params['employeeId'];
@@ -37,27 +29,41 @@ export class DashboardComponent implements OnInit {
     this.getNumberOfPages();
   }
 
-  
-
   getNumberOfPages() {
     return this.service.getTicketByUserId(this.employeeId).subscribe({
-      next : (data: number) => {
-        console.log(data)
+      next: (data: number) => {
+        console.log(data);
         this.num = Math.ceil(data / 5);
-        console.log(this.num)
+        console.log(this.num);
         if (this.num > 0) {
-                for (let i = 0; i < this.num; i++) {
-                  this.button = {
-                    value: i,
-                    min: 1 + 5 * i,
-                    max: 5 + 5 * i,
-                  };
-                  this.buttons.push(this.button);
-                  console.log('sto aggiungendo questo bottone: ', this.button);
-                }
-              }
+          for (let i = 0; i < this.num; i++) {
+            this.button = {
+              value: i,
+              min: 1 + 5 * i,
+              max: 5 + 5 * i,
+            };
+            this.buttons.push(this.button);
+            console.log('sto aggiungendo questo bottone: ', this.button);
+          }
+        }
       },
-      error : (err:any) => {
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
+  }
+
+  getPageTickets(min:number, max:number) {
+    return this.service.pageTicket({
+      employeeId: this.employeeId,
+      min: min, 
+      max: max
+    }).subscribe({
+      next : (data: any) => {
+        console.log(data)
+        
+      },
+      error: (err:any) => {
         console.log(err)
       }
     })
--- src/app/services/ticket.service.ts
+++ src/app/services/ticket.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Ticket } from '../interfaces/ticket';
 @Injectable({
   providedIn: 'root'
 })
@@ -8,9 +9,20 @@ export class TicketService {
 
   constructor(private http: HttpClient) { }
 
-  url = "http://localhost:8080/api/tickets/"
+  url = "http://localhost:8080/api/"
 
   getTicketByUserId(userId: number): Observable<number> {
-    return this.http.get<number>(this.url + userId)
+    return this.http.get<number>(this.url + "tickets/" + userId)
   }
+
+  pageTicket(body: {employeeId: number; min:number; max:number}) : Observable<Ticket[]> {
+    return this.http.post<Ticket[]>(this.url + "ticket/ricerca", body);
+  }
+
+  // loginEmployee(body: { email: string; password: string }): Observable<Employee> {
+  //   return this.http.post<Employee>("http://localhost:8080/api/login", body);
+  // }
+
+
+
 }
